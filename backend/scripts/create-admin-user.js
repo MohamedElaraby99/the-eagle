c3e3f76405538391560c87b4ee2713c5ad8bd1f2
Mohamed El Araby <mohamedalarbic6@gmail.com>
2025-08-19 16:15:24 +0300

    SD

--- backend/scripts/create-admin-user.js
+++ backend/scripts/create-admin-user.js
@@ -32,7 +32,7 @@ const createAdminUser = async () => {
     const adminUser1 = {
       username: 'adminn',
       fullName: 'System Administrator',
-      email: 'adminn@api.com',
+      email: 'adminn@lms.com',
       password: '1234567', // Will be hashed by the pre-save middleware
       role: 'ADMIN',
       isActive: true
